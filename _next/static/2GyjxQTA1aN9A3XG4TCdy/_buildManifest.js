c989c941e13f318320b7fef742cec319589e5bf7
tcpglobalc <tcpglobal@protonmail.com>
2023-07-20 02:27:12 +0800

    Updates

---
+++ _next/static/2GyjxQTA1aN9A3XG4TCdy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-94f50ba15a15c0ff.js","static/chunks/877-dc8bb4d59d8f0ddf.js","static/chunks/pages/index-dfe950f8a578443b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
